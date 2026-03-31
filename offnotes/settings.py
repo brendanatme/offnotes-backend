897d8fe3a7fd79b3dff04832b8224babd30ef94d
brendanatme <brendanm@hey.com>
2026-03-31 11:53:57 -0400

    use postgres on prod

--- offnotes/settings.py
+++ offnotes/settings.py
@@ -99,6 +99,14 @@ DATABASES = {
     }
 }
 
+if os.environ.get("DATABASE_URL"):
+    import dj_database_url
+
+    DATABASES["default"] = dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
